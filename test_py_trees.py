cd97d4488ea63a6d1fc51a816c1ea542b0a6892a
f4lcini <falcinivincenzo@gmail.com>
2026-05-05 21:37:06 +0200

    cleaning

--- test_py_trees.py
+++
@@ -1,6 +0,0 @@
-import py_trees
-from py_trees.display import unicode_tree
-b = py_trees.behaviours.Success(name="MySuc")
-b.tick_once()
-b.stop(py_trees.common.Status.INVALID)
-print(unicode_tree(b, show_status=True))
